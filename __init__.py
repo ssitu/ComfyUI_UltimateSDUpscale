79e0e3b496b0d1891d6d01fb663e48a5bfddf6b5
ssit <ssit5568@gmail.com>
2023-08-15 17:22:12 -0400

    Refactoring

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ repo_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, repo_dir)
 original_modules = sys.modules.copy()
 
-# Place aside existing modules if using a1111 web ui
+# Place aside potentially conflicting modules
 modules_used = [
     "modules",
     "modules.devices",
@@ -15,10 +15,10 @@ modules_used = [
     "modules.upscaler",
     "utils",
 ]
-original_webui_modules = {}
+original_imported_modules = {}
 for module in modules_used:
     if module in sys.modules:
-        original_webui_modules[module] = sys.modules.pop(module)
+        original_imported_modules[module] = sys.modules.pop(module)
 
 # Proceed with node setup
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
@@ -36,4 +36,4 @@ for module in modules_to_remove:
     del sys.modules[module]
 
 # Restore original modules
-sys.modules.update(original_webui_modules)
+sys.modules.update(original_imported_modules)
